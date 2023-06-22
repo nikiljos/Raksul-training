feb525c4f05e2de0b8d9be3346d87a42e8e64b94
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-06-22 17:59:15 +0530

    Complete Google OAuth authentication on front-end and back-end both

--- client/src/components/Navbar/NavButton/NavButton.tsx
+++ client/src/components/Navbar/NavButton/NavButton.tsx
@@ -1,7 +1,12 @@
 import "./NavButton.css";
+import { Link } from "react-router-dom";
 
 function NavButton() {
-  return <button className="nav-btn">Login</button>;
+  return (
+    <Link to="/login">
+      <button className="nav-btn">Login</button>
+    </Link>
+  );
 }
 
 export default NavButton;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <BrowserRouter>
-    <App />
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_OAUTH_CID as string}>
+      <App />
+    </GoogleOAuthProvider>
   </BrowserRouter>
 );
