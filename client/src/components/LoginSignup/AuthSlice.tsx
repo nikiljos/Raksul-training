ff4cc35c72dc4686719c6e482a92088d82465410
nikiljos <nikhiljose12345@gmail.com>
2023-06-29 01:09:58 +0530

    feat:redux,auth state

---
+++ client/src/components/LoginSignup/AuthSlice.tsx
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authInitial = {
+  token: null,
+  user: {
+    id: null,
+    name: null,
+    image: null,
+    email: null,
+  },
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: authInitial,
+  reducers: {
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    unsetUser: (state) => {
+      state.token = null;
+      state.user = authInitial.user;
+    },
+  },
+});
+
+export const { setToken, setUser, unsetUser } = authSlice.actions;
+
+export default authSlice.reducer;
--- client/src/components/LoginSignup/GAuthLogin.tsx
+++ client/src/components/LoginSignup/GAuthLogin.tsx
@@ -1,6 +1,9 @@
 import { GoogleLogin } from "@react-oauth/google";
+import { useAppDispatch } from "../../hooks";
+import { setToken } from "./AuthSlice";
 
 function GAuthLogin() {
+  const dispatch = useAppDispatch();
   const handleGoogleLoginSuccess = (tokenId: any) => {
     fetch(`${process.env.REACT_APP_SERVER_URL}/api/auth/google`, {
       method: "POST",
@@ -13,6 +16,7 @@ function GAuthLogin() {
       .then((data) => {
         if (data) {
           localStorage.setItem("access_token", data.data.authToken);
+          dispatch(setToken(data.data.authToken));
         }
         console.log("Message:", data);
       })
