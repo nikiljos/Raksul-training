2e6ff55a421d83c95b6b92df02711ba9ff874273
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-07-02 20:53:51 +0530

    Practice of Redux Toolit

--- Practice/react-adv/src/App.tsx
+++ Practice/react-adv/src/App.tsx
@@ -1,16 +1,23 @@
 import "./App.css";
-// import Todo from "./components/Reducer/TodoForm";
-
-import CounterApp from "./components/Redux/CounterApp";
-import { store } from "./components/Redux/store";
+import CounterWithTLK from "./components/ReduxToolkit/CounterWithTLK";
 import { Provider } from "react-redux";
+import { store } from "./components/ReduxToolkit/store";
+
+// import CounterApp from "./components/Redux/CounterApp";
+// import { store } from "./components/Redux/store";
+// import { Provider } from "react-redux";
+
+// import Todo from "./components/Reducer/TodoForm";
 
 function App() {
   return (
     <div className="App">
       {/* <Todo /> */}
-      <Provider store={store}>
+      {/* <Provider store={store}>
         <CounterApp />
+      </Provider> */}
+      <Provider store={store}>
+        <CounterWithTLK />
       </Provider>
     </div>
   );
