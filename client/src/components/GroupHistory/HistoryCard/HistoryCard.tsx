99b82c2f78294a134a275800f4ef4275c730537e
Raju Tukadiya <tukadyaraju17@gmail.com>
2023-07-11 16:38:50 +0530

    Make share button working

--- client/src/components/GroupHistory/HistoryCard/HistoryCard.tsx
+++ client/src/components/GroupHistory/HistoryCard/HistoryCard.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import "./HistoryCard.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 type Props = {
   name: string;
@@ -19,27 +21,41 @@ function HistoryCard({ name, id, date, invite_code }: Props) {
     return date.toLocaleDateString("en-IN", options);
   }
 
+  const copyURL = (id: number) => {
+    navigator.clipboard
+      .writeText(`${process.env.REACT_APP_SITE_URL}/group/${id}`)
+      .then(() => {
+        toast.success("Link copied to clipboard!");
+      })
+      .catch((error) => {
+        toast.error("Failed to copy URL to clipboard:");
+      });
+  };
+
   return (
-    <div className="history-card">
-      <div className="hc-content">
-        <div className="title">{name}</div>
-        <div className="invite-code">
-          <b>Code:</b> {invite_code}
+    <>
+      <div className="history-card">
+        <div className="hc-content">
+          <div className="title">{name}</div>
+          <div className="invite-code">
+            <b>Code:</b> {invite_code}
+          </div>
+          <div className="date">{formatDate(date)}</div>
         </div>
-        <div className="date">{formatDate(date)}</div>
+        <div className="hc-btn-container">
+          <div className="hc-btn share" onClick={() => copyURL(id)}>
+            <img src="/images/share.svg" alt="Share" />
+          </div>
+          <Link to={`/group/${id}`} className="hc-btn view">
+            <img src="/images/view.svg" alt="View" />
+          </Link>
+          <div className="hc-btn file">
+            <img src="/images/file.svg" alt="Export" />
+          </div>
+        </div>
       </div>
-      <div className="hc-btn-container">
-        <div className="hc-btn share">
-          <img src="/images/share.svg" alt="Share" />
-        </div>
-        <Link to={`/group/${id}`} className="hc-btn view">
-          <img src="/images/view.svg" alt="View" />
-        </Link>
-        <div className="hc-btn file">
-          <img src="/images/file.svg" alt="Export" />
-        </div>
-      </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 }
 
